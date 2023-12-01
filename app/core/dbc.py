16c3e66fc942de6f7230d0a28853da6d4ffc2131
Kuttudb <92996439+Kuttudb@users.noreply.github.com>
2023-12-01 08:03:46 +0000

    Fixed

--- app/core/dbc.py
+++ app/core/dbc.py
@@ -1,9 +1,11 @@
-from sqlalchemy import  MetaData
+from sqlalchemy import MetaData
 from databases import Database
 from .config import CONFIG
 
 try:
     db = Database(CONFIG.DATABASE_URL)
-except:
-    raise('Invalid or missing database url')
+    
+except Exception as e:
+    raise Exception('Invalid or missing database URL') from e
+
 metadata = MetaData()
