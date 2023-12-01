1e98d1d81ab362c58413bf379fe035a93bd76080
H21code <92996439+Kuttudb@users.noreply.github.com>
2023-12-01 14:00:39 +0530

    Update tgapp.py

--- app/tg/tgapp.py
+++ app/tg/tgapp.py
@@ -4,7 +4,7 @@ from typing import Union
 from app.core import CONFIG
 from .utils import is_allowed_grp
 
-client = TelegramClient(  StringSession( CONFIG.SESSION_STRING, CONFIG.TG_APP_ID, CONFIG.TG_APP_HASH)
+client = TelegramClient(  StringSession( CONFIG.SESSION_STRING), CONFIG.TG_APP_ID, CONFIG.TG_APP_HASH)
 client.parse_mode = 'md'
 
 
